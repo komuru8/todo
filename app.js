45c1e8ea87befdbac33fce9682dc19bc776e8339
小林篤史 <kobayashiatsushi@kobayashiatsushinoiMac.local>
2025-04-19 11:45:27 +0900

    mod URL

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use("/api/v1/tasks", taskRoute);
 //データベースと接続
 const start = async() => {
     try {
-        await connectDB(process.env.MONGO_URI);
+        await connectDB(process.env.MONGO_render_URL || process.env.MONGO_URI);
         //問題なく立ち上がったらメッセージを表示
     app.listen(process.env.PORT || PORT, console.log("サーバーが起動しました"));
     }catch (err){
